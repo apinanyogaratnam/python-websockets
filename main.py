e81ae60ab8de7d906386916eaaab3feda06104a8
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-29 18:37:31 -0400

    refactor: allowing connections from multiple clients

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import asyncio
 import websockets
 
+connections = set()
+
 
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(f'received message: {message}')
+    connections.add(websocket)
+    try:
+        async for message in websocket:
+            for connection in connections:
+                if connection != websocket:
+                    await connection.send(
+                        f'received message from connection {list(connections).index(connection)}: {message}'
+                    )
+    finally:
+        connections.remove(websocket)
 
 
 start_server = websockets.serve(echo, '0.0.0.0', 8000)
